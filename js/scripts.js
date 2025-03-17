9740b92a852e8238be735a7808193352dd21e37b
Samuel Jayasingh <samueljayasingh77@gmail.com>
2025-03-18 00:59:07 +0530

    fix: Optimised the HomeNav

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,63 @@
-
-window.addEventListener('DOMContentLoaded', event => {
-
+window.addEventListener("DOMContentLoaded", (event) => {
+    var clicked = false;
     // Navbar shrink function
     var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
+      const navbarCollapsible = document.body.querySelector("#mainNav");
+      if (!navbarCollapsible) {
+        return;
+      }
+      if (window.scrollY <= 50 && !clicked) {
+        navbarCollapsible.classList.remove("navbar-shrink");
+      } else {
+        navbarCollapsible.classList.add("navbar-shrink");
+      }
     };
-
-    // Shrink the navbar 
+  
+    // Shrink the navbar
     navbarShrink();
-
+  
     // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
+    document.addEventListener("scroll", navbarShrink);
+  
     // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
+    const mainNav = document.body.querySelector("#mainNav");
     if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
+      new bootstrap.ScrollSpy(document.body, {
+        target: "#mainNav",
+        offset: 74,
+      });
+    }
+  
     // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
     const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
+      document.querySelectorAll("#navbarResponsive .nav-link")
     );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+  
+    // Handle navbar toggler click
+    navbarToggler.addEventListener("click", () => {
+      setTimeout(() => {
+        if (navbarToggler.classList.contains("collapsed") && window.scrollY <= 50) {
+          setTimeout(() => {
+            mainNav.classList.remove("navbar-shrink");
+            clicked = false;
+          }, 300);
+        } else {
+          mainNav.classList.add("navbar-shrink");
+          clicked = true;
+        }
+      }, 10);
     });
-
-    
-});
-
-
+  
+    responsiveNavItems.forEach((responsiveNavItem) => {
+      responsiveNavItem.addEventListener("click", () => {
+        if (window.getComputedStyle(navbarToggler).display !== "none") {
+          navbarResponsive.classList.remove("navbar-shrink");
+          setTimeout(() => {
+            navbarToggler.click();
+          }, 300);
+        }
+      });
+    });
+  });
+  
--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,26 @@
-$('.team-slider').owlCarousel({
-	loop: true,            
-	nav: false,
+$(".team-slider").owlCarousel({
+	loop: true,
+	nav: true,
 	autoplay: true,
 	autoplayTimeout: 5000,
 	smartSpeed: 450,
 	margin: 20,
 	responsive: {
-		0: {
-			items: 1
-		},
-		768: {
-			items: 2
-		},
-		991: {
-			items: 3
-		},
-		1200: {
-			items: 3
-		},
-		1920: {
-			items: 3
-		}
-	}
-});
+	  0: {
+		items: 1,
+	  },
+	  768: {
+		items: 2,
+	  },
+	  991: {
+		items: 3,
+	  },
+	  1200: {
+		items: 3,
+	  },
+	  1920: {
+		items: 3,
+	  },
+	},
+  });
+  
